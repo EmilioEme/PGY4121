d82c50dd4529490b950218d0b0d724bb991ce052
EmilioEme <87208363+EmilioEme@users.noreply.github.com>
2021-09-30 00:00:56 -0300

    Agregado Login
    Se agrego login con validacion

--- GGStore/src/app/login/login.page.ts
+++ GGStore/src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Navigation } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
   }
 
+  login(form) {
+    
+    var usuario = form.value ["name"]
+    var contrasena = form.value ["password"]
+
+    if(usuario == "admin" && contrasena == "123"){
+       
+      localStorage.setItem("datosUsuario", usuario);
+      this.router.navigate(['/home'])
+    }
+  }
 }
