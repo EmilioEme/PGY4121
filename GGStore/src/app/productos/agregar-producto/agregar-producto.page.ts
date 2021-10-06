cdd91ad97d2eb48a2b2f5c69792b8bf29de2de6a
EmilioEme <87208363+EmilioEme@users.noreply.github.com>
2021-10-06 07:53:08 -0300

    Fix agregar productos
    Algunos pequeños arreglos en agregar productos

--- GGStore/src/app/productos/agregar-producto/agregar-producto.page.ts
+++ GGStore/src/app/productos/agregar-producto/agregar-producto.page.ts
@@ -15,9 +15,24 @@ export class AgregarProductoPage implements OnInit {
   }
 
   agregarProducto(nombre,url,anio,genero,desarrolladora,distribuidora, comentario){
-    var lista = comentario.value.split(",")
-    var listaGenero = genero.value.split(",")
+    var lista
+    var listaGenero
+    
+    if(comentario!=null){
+      if(comentario.value != ""){
+        lista = comentario.value.split(",")
+      }else{
+        lista = null;
+      }
+    }
 
+    if(genero!=null){
+      if(genero.value != ""){
+        listaGenero = genero.value.split(",")
+      }else{
+        listaGenero = null;
+      }
+    }
     this.productoServicio.addProductos(nombre.value ,url.value,anio.value,listaGenero,desarrolladora.value,distribuidora.value, lista);
     this.router.navigate(['/productos'])
   }
